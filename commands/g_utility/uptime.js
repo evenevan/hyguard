a690b52b3c1231dd84000062d63b5a1f76c030c5
BuildTools <unconfigured@null.spigotmc.org>
2021-07-09 20:10:34 -0700

    uptime fixed

--- commands/g_utility/uptime.js
+++ commands/g_utility/uptime.js
@@ -7,15 +7,15 @@ module.exports = {
   cooldown: 5,
 	execute(message, args, client) {
 		try {
-			function uptimefunc(uptimeSeconds) {
+			function uptimeFunc() {
 				let totalSeconds = (client.uptime / 1000);
-				var roundedNumber = Math.round(totalSeconds * 10) / 10;
-					  var pureDays = Math.floor(uptimeSeconds / (3600 * 24));
-					  var uptimeDays = pureDays > 0 ? pureDays + (pureDays == 1 ? ' day ' : ' days ') : ''; //checks if day or days should be used
-				var hmsuptime = new Date(totalSeconds * 1000).toISOString().substr(11, 8);
+				let roundedNumber = Math.round(totalSeconds * 10) / 10;
+				let pureDays = Math.floor(roundedNumber / (3600 * 24));
+				let uptimeDays = pureDays > 0 ? pureDays + (pureDays == 1 ? ' day ' : ' days ') : ''; //checks if day or days should be used
+				let hmsuptime = new Date(totalSeconds * 1000).toISOString().substr(11, 8);
 					  return `:stopwatch: Current uptime is ${uptimeDays}${hmsuptime}, or ${roundedNumber} seconds.`;
 					  }
-				message.channel.send(uptimefunc());
+				message.channel.send(uptimeFunc());
 		} catch (err) {
 			console.log(`Error 11: ${err}`);
 			message.channel.send(`An unknown error occured. Please report this. ERROR_11: \`${err}\``);
